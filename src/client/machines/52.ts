1693906f02ae8aec88166225fb3be4d1d7dabf49
Toon van Strijp <toonvanstrijp@gmail.com>
2020-09-02 14:41:42 +0200

    close alert

--- src/client/machines/52.ts
+++ src/client/machines/52.ts
@@ -1,16 +1,25 @@
 import { Machine } from "./base";
 import { listen } from "../../common/decorators/listen.decorator";
 import { PeerMakeAlert } from "../../common/messages/makeAlert.peer";
+import { PeerCloseAlert } from "../../common/messages/closeAlert.peer";
 
 export default class RGM52 extends Machine {
   readonly id = 52;
+
+  messageBox: HTMLElement = document.getElementById("message");
 
   onStart(): void {
     alert("52 was triggered!");
   }
 
   @listen(PeerMakeAlert)
-  onAlert(message: PeerMakeAlert) {
-    alert(message.message);
+  onAlert({ message }: PeerMakeAlert) {
+    this.messageBox.classList.add("active");
+    this.messageBox.innerHTML = message;
+  }
+
+  @listen(PeerCloseAlert)
+  onCloseAlert() {
+    this.messageBox.classList.remove("active");
   }
 }
