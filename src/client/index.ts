bdbbbffce698a4c82e999465f81740134be559d0
Toon van Strijp <toonvanstrijp@gmail.com>
2020-09-02 14:15:19 +0200

    initial design, query param read

--- src/client/index.ts
+++ src/client/index.ts
@@ -2,7 +2,8 @@ import SocketIO from "socket.io-client";
 import { ServerHandler } from "./server.handler";
 import { Machine } from "./machines/base";
 
-const rgmID = 51;
+const rgmID = parseInt(getQueryVariable("id"));
+
 type Klass<T> = new (...args: any[]) => T;
 
 /**
@@ -29,4 +30,17 @@ const bootstrap = async () => {
   const Machine = await loadMachine(rgmID);
   const machine = new Machine(socket);
 };
+
+function getQueryVariable(variable: string): string {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (decodeURIComponent(pair[0]) == variable) {
+      return decodeURIComponent(pair[1]);
+    }
+  }
+  console.log("Query variable %s not found", variable);
+}
+
 bootstrap();
