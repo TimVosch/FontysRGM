c8c3ddbf9ba1fd35e37d69ec3e36e0a6d6ba1e9d
Tim van Osch <timvosch@pollex.nl>
2020-09-03 12:58:13 +0200

    Add test producing for viewer

--- src/client/viewer/viewer.component.tsx
+++ src/client/viewer/viewer.component.tsx
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import { MessageHandler } from "../message.handler";
 import { ClientRegisterRGM } from "../../common/messages/registerRGM.client";
 import { ClientElbowshake } from "../../common/messages/elbowshake.client";
+import * as mediasoup from "mediasoup-client";
+import { listen } from "../../common/decorators/listen.decorator";
+import { ClientRequestTransport } from "../../common/messages/rtc/requestTransport.client";
+import { ServerRequestTransport } from "../../common/messages/rtc/requestTransport.server";
 
 interface ViewerProps {
   socket: SocketIOClient.Socket;
@@ -9,6 +13,9 @@ interface ViewerProps {
 
 export class Viewer extends Component<ViewerProps> {
   handler: MessageHandler;
+  rtcDevice: mediasoup.types.Device;
+  rtcSendTransport: mediasoup.types.Transport;
+  rtcRecvTransport: mediasoup.types.Transport;
 
   constructor(props: Readonly<ViewerProps>) {
     super(props);
@@ -16,7 +23,18 @@ export class Viewer extends Component<ViewerProps> {
     this.handler = new MessageHandler(this, props.socket);
   }
 
-  componentDidMount() {
+  /**
+   * Initialize objects required for WebRTC
+   */
+  initializeRTC() {
+    const req = new ClientRequestTransport();
+    this.handler.send(req);
+  }
+
+  /**
+   * Initialize websocket connection with server
+   */
+  initializeWS() {
     this.handler.open();
 
     const elbowshakeMSG = new ClientElbowshake();
@@ -28,8 +46,65 @@ export class Viewer extends Component<ViewerProps> {
     this.handler.send(registerMSG);
   }
 
+  componentDidMount() {
+    this.initializeWS();
+    this.initializeRTC();
+  }
+
   componentWillUnmount() {
     this.handler.close();
+  }
+
+  @listen(ServerRequestTransport)
+  async onClientRequestTransport(message: ServerRequestTransport) {
+    console.log(`Received RTP Caps`);
+    debugger;
+
+    // Create mediasoup device
+    this.rtcDevice = new mediasoup.Device();
+
+    await this.rtcDevice.load({
+      routerRtpCapabilities: message.rtpCapabilties,
+    });
+
+    if (!this.rtcDevice.canProduce("video")) {
+      console.warn(`This device cannot produce video`);
+      return;
+    }
+
+    this.rtcSendTransport = this.rtcDevice.createSendTransport({
+      id: message.id,
+      iceCandidates: message.iceCandidates,
+      iceParameters: message.iceParameters,
+      dtlsParameters: message.dtlsParameters,
+      sctpParameters: message.sctpParameters,
+      iceServers: [],
+    });
+    this.rtcSendTransport.on("connectionstatechange", (connectionState) => {
+      console.log(`RTC Transport ${connectionState}`);
+    });
+
+    if (!this.rtcDevice.canProduce("video")) {
+      console.warn("RTC device cannot produce video");
+    }
+
+    // Create transport
+    const mediaStream = await navigator.mediaDevices.getUserMedia({
+      video: { source: "device" },
+    } as any);
+    const track = mediaStream.getVideoTracks()[0];
+    const producer = await this.rtcSendTransport.produce({
+      track,
+      encodings: [
+        {
+          maxBitrate: 10000,
+          maxFramerate: 15,
+        },
+      ],
+      codecOptions: {
+        videoGoogleStartBitrate: 1000,
+      },
+    });
   }
 
   render() {
--- src/server/viewer.handler.ts
+++ src/server/viewer.handler.ts
@@ -1,9 +1,14 @@
+import * as mediasoup from "mediasoup";
 import { MessageHandler } from "./message.handler";
 import { ClientRegisterRGM } from "../common/messages/registerRGM.client";
 import { listen } from "../common/decorators/listen.decorator";
+import { ClientRequestTransport } from "../common/messages/rtc/requestTransport.client";
+import { RTCServer } from "./rtcserver";
+import { ServerRequestTransport } from "../common/messages/rtc/requestTransport.server";
 
 export class ViewerHandler {
   private readonly handler: MessageHandler;
+  private transport: mediasoup.types.WebRtcTransport;
 
   constructor(client: SocketIO.Socket) {
     this.handler = new MessageHandler(this, client);
@@ -11,6 +16,29 @@ export class ViewerHandler {
 
   @listen(ClientRegisterRGM)
   onRegister(message: ClientRegisterRGM) {
-    console.log(`Viewer client is registering ${message.id}`);
+    console.log(`[ViewerHandler] Client connected ${message.id}`);
+  }
+
+  @listen(ClientRequestTransport)
+  async onTransportRequest(message: ClientRequestTransport) {
+    const msg = new ServerRequestTransport();
+    msg.rtpCapabilties = RTCServer.getRTPCapabilities();
+
+    this.transport = await RTCServer.createRTCTransport();
+
+    this.transport.on("connect", () => {
+      console.log(`[ViewerHandler] Client connected to transport`);
+    });
+    this.transport.on("produce", () => {
+      console.log(`[ViewerHandler] Client produced on transport`);
+    });
+
+    msg.id = this.transport.id;
+    msg.iceCandidates = this.transport.iceCandidates;
+    msg.iceParameters = this.transport.iceParameters;
+    msg.dtlsParameters = this.transport.dtlsParameters;
+    msg.sctpParameters = this.transport.sctpParameters;
+
+    this.handler.send(msg);
   }
 }
